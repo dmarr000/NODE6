830c1305e588f96874f1e45d09cdab6cb097cbcd
Derrick Marr <dmarr000@yahoo.com>
2021-06-24 23:22:17 -0400

    Mongoose Part 2

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const connect = mongoose.connect(url, {
 connect.then(() => {
     console.log('Connection to server established');
 
-    const newCampsite = new Campsite({
+    //const newCampsite = new Campsite({   This line is an alterate way to instantiate the model. The below method automatically saves it, so that has also been commented out below.
+    Campsite.create({
         name: 'React Lake Campground',
         description: 'test'
-    });
-
-    newCampsite.save()
+    })
+    //newCampsite.save()
     .then(campsite => {
         console.log(campsite);
         return Campsite.find();
